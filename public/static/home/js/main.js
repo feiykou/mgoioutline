5c2e01da427439a687280a5dca46b49d12fee548
feiy <feiy@163.com>
2019-02-27 14:58:53 +0800

    唔该

--- public/static/home/js/main.js
+++ public/static/home/js/main.js
@@ -22,3 +22,19 @@ var fnJsLoad = function(url, callback) {
 
     document.getElementsByTagName('head')[0].appendChild(eleScript);
 };
+
+
+function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+        "SymbianOS", "Windows Phone",
+        "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
