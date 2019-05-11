13860fc562bff7c5b6a1655e2a37403ab639a977
feiy <feiy@163.com>
2019-05-11 17:30:26 +0800

    唔该

--- application/api/route.php
+++ application/api/route.php
@@ -8,16 +8,17 @@ Route::get('api/:version/index/newsProduct','api/:version.Index/newsProduct');
 Route::get('api/:version/index/topCate','api/:version.Index/topCate');
 Route::get('api/:version/index/column','api/:version.Index/getIndexColumn');
 
+
 Route::get('api/:version/column/detail/:id','api/:version.Column/detail');
-Route::get('api/:version/cate/all','api/:version.Cate/getAllCate');
+Route::get('api/:version/column/:cate_id','api/:version.Column/getColumnByCateId',[],['cate_id'=>'\d+']);
+Route::get('api/:version/column/top_cate','api/:version.Column/getTopCate');
+Route::get('api/:version/columnCate/:id','api/:version.ColumnCate/getColumnCateById');
+
 
 
 Route::get('api/:version/product/:cate_id','api/:version.Product/lst');
-Route::get('api/:version/column/:cate_id','api/:version.Column/getColumnByCateId',[],['cate_id'=>'\d+']);
-Route::get('api/:version/column/top_cate','api/:version.Column/getTopCate');
-
 
 // 公共
 Route::get('api/:version/cate/second','api/BaseController/cate');
-
+Route::get('api/:version/cate/all','api/:version.Cate/getAllCate');
 
